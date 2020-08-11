7268704822bb53818ee002d00a18924e52d0cafb
Thiago da Silva Martins <thiagod.smartins@icloud.com>
2020-08-10 21:04:15 -0400

    Initial Commit

---
+++ ARPlanets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARPlanets
+//
+//  Created by Thiago Martins on 10/08/20.
+//  Copyright © 2020 ThiagoMartins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
